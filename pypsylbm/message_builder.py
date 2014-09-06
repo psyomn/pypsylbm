ba55e41f2e70e7d6f497084064aa0ab26509cf13
psyomn <lethaljellybean@gmail.com>
2014-09-06 10:08:05 -0400

    First commit

---
+++ pypsylbm/message_builder.py
@@ -0,0 +1,12 @@
+from pypsylbm import user
+
+class MessageBuilder(object):
+    """ 
+      Building text messages that will later be used in order to wrap
+      the protocol stuff
+    """
+    
+    def __init__(self, user):
+        self.__user = user
+
+
---
+++ pypsylbm/user.py
@@ -0,0 +1,16 @@
+
+class User(object): 
+  """ User of the specific account """
+
+    def __init__(self, name):
+        self.__name = name
+        self.__api_token = None
+
+    @property
+    def api_token(self):
+        return self.__api_token
+      
+    @property
+    def name(self):
+        return self.__name
+
