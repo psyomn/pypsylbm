d6542cd7287229f84d1e0bd6c72a0a3293e0505b
Simon <lethaljellybean@gmail.com>
2014-09-20 15:12:32 -0400

    delete: implement stubs for protocol

---
+++ pypsylbm/commands/delete.py
@@ -0,0 +1,11 @@
+import socket
+
+from pypsylbm.host import Host
+
+class Delete(object):
+    
+    def __init__(self, book):
+        self._book = book
+        
+    def execute(self):
+        pass
